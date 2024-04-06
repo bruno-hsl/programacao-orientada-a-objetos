28cc184a3dd1eee8141f1e54bf77d6850bb77400
bruno.hsl <bruno.hsl.007@gmail.com>
2024-04-06 11:24:17 -0300

    livraria

---
+++ livraria/Livro.java
@@ -0,0 +1,19 @@
+package livraria;
+
+public class Livro {
+
+    int id;
+    String titulo;
+    String nomeAutor;
+    String anoPublicao;
+    String editora;
+
+        public Livro(int id, String titulo, String nomeAutor, String anoPublicacao, String editora){
+            this.id = id;
+            this.titulo = titulo;
+            this.nomeAutor = nomeAutor;
+            this.anoPublicao = anoPublicacao;
+            this.editora = editora;
+        }
+    
+}
